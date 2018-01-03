b9af28870c578479964f178625b2650e8dbd25e4
amandeepmittal <amandeepmittal@live.com>
2018-01-03 16:46:05 +0530

    add new page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <br />
+    <Link to="/page-3">New Page!</Link>
   </div>
-)
+);
 
-export default SecondPage
+export default SecondPage;
